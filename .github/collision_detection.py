e93b2070c7b3608c2fc1d992a7355255332c1ba8
Antonio Gonzales <basketballboy26771@gmail.com>
2022-01-25 09:07:05 -0500

    1A version 0.1

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,1 +1,8 @@
-#PyGame Collision Detection, Antonio Gonzales, January 25, 2022,8:47am, v0.0 
+#PyGame Collision Detection, Antonio Gonzales, January 25, 2022,9:7am, v0.1 
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup PyGame 
+pygame.init()
+mainClock = pygame.time.Clock()
