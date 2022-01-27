72b81d211e7676e8f8e0b921083699612d01bc84
Antonio Gonzales <s11165677@icloud.com>
2022-01-27 09:21:41 -0500

    1A (0.5)

--- .github/collision_detection.py
+++ .github/collision_detection.py
@@ -1,4 +1,4 @@
-#PyGame Collision Detection, Antonio Gonzales, January 27, 2022,9:14am, v0.4
+#PyGame Collision Detection, Antonio Gonzales, January 27, 2022,9:20am, v0.5
 
 import pygame, sys, random
 from pygame.locals import *
@@ -27,3 +27,11 @@ foods = []
 
 for i in range(20):
     foods.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWWIDTH - FOODSIZE), FOODSIZE, FOODSIZE))
+
+# Movement VAriables 
+moveLeft = False
+moveRight = False
+moveUp = False 
+moveDown = False 
+
+MOVESPEED = 6
